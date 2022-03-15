f04dd392c0a7459c2e11dc75d470a39c314e5b5c
Abdullah Baaboud <Baaboud.dev@gmail.com>
2022-03-15 11:51:46 +0300

    Revert "select"

--- home.php
+++ home.php
@@ -5,31 +5,16 @@ class HomePage{
     public $dbObject;
     function __construct()
     {
-        $this->dbObject=new DB('e-store');
+        $this->dbObject=new DB();
         
     }
-
-
-
 }
 
-
-
 $home=new HomePage();
-$result=$home->dbObject->selectById("product",$_GET['book_id']);
-$aa = $home->dbObject->selectAll('product');
-$ss = $home->dbObject->select(['product'],['name','price'],['id'=>'41','quantity'=>'550'],false);
+$result=$home->dbObject->select('books',$_GET['id']);
+
 foreach($result as $r)
 {
     echo $r->name."". $r->price;
-}
-foreach($aa as $s)
-{
-    echo $s->name."". $s->price;
-}
-
-foreach($ss as $s)
-{
-    echo $s->name . $s->price;
 }
 ?>
